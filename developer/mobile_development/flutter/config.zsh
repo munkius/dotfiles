57e43597d1dd19b4f1362e9861e097dd1c9db24d
munkius <194593+munkius@users.noreply.github.com>
2022-05-10 10:16:35 +0200

    updated config for fastlane

--- developer/mobile_development/flutter/config.zsh
+++ developer/mobile_development/flutter/config.zsh
@@ -1,2 +1,6 @@
 export CHROME_EXECUTABLE=/Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser
 export JAVA_HOME=$(/usr/libexec/java_home)
+export FLUTTER_ROOT=$(asdf where flutter)
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
