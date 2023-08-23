945118185fc63723107f071f5f2f4f09b9d7f446
munkius <194593+munkius@users.noreply.github.com>
2023-08-23 13:22:59 +0200

    Fixing JAVA_HOME for Visual Studio

--- developer/mobile_development/flutter/config.zsh
+++ developer/mobile_development/flutter/config.zsh
@@ -1,5 +1,5 @@
 export CHROME_EXECUTABLE=/Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser
 export FLUTTER_ROOT=$(asdf where flutter)
-
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
