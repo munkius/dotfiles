3c0bfcc600c0bc2a7a1a4360adad9cb94c867b32
munkius <194593+munkius@users.noreply.github.com>
2026-05-21 14:42:51 +0200

    Sync local zsh and n8n shell config

--- developer/n8n/nvm.zsh
+++ developer/n8n/nvm.zsh
@@ -1,4 +1,5 @@
 export N8N_RUNNERS_ENABLED=true
+export GENERIC_TIMEZONE=Europe/Amsterdam
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
